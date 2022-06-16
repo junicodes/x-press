5e55c9a10408cb59b8188bb2098ced747cd19cda
Okechukwu Obi <okechukwuobicode@gmail.com>
2022-06-16 17:56:07 +0100

    fix

--- src/components/layouts/elements/Header.tsx
+++ src/components/layouts/elements/Header.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import { Navigate, useNavigate } from 'react-router-dom'
 import { DefaultButton, Logo } from '../../sub-elements'
 
 const Header = () => {
 
-  const handleGotoAuth = () => {
 
+   //navigation
+   const navigate = useNavigate();
+
+  const handleGotoAuth = () => {
+    window.location.pathname === '/' ? navigate("/sign-in") : navigate("/")
   }
+
+  console.log(window.location.pathname, 'pathnname')
 
   return (
     <div className='py-10 sm:py-0 sm:h-36 flex flex-col sm:flex-row sm:justify-between items-center w-full'>
       <Logo />
       <div className='flex flex-row justify-start items-center pt-5 sm:pt-0'>
-        <p className='mr-2 text-custom-gray text-xs md:text-sm'>Already have an account?</p>
+        <p className='mr-2 text-custom-gray text-xs md:text-sm'>
+          {
+            window.location.pathname == '/' && 'Already have an account?'
+          }
+          {
+            window.location.pathname == '/sign-in' && 'New to Xpress Rewards?'
+          }
+          
+        </p>
         <DefaultButton
-          labelText="Sign in"
+          labelText={window.location.pathname === '/' ? 'Sign In' : 'Sign Up'}
           handleClick={handleGotoAuth}
           variant="border w-full text-sm font-semibold rounded-sm border-primary text-primary py-2 px-3 flex flex-row justify-center item-center"
           containerVariant="relative"
